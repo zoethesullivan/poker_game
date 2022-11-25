ab2af456b6b335418ced98f5992b660f0d7ef952
Zoe <70442338+zoe-moment@users.noreply.github.com>
2022-11-25 15:22:31 -0800

    add JS file

---
+++ js.js
@@ -0,0 +1,18 @@
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+if (sum <= 20) {
+  message = "Do you want to draw a new card?"
+} else if (sum === 21) {
+  message = "You WIN!"
+  hasBlackJack = true
+} else {
+  message = "Busted!"
+  isAlive = false
+}
+
+console.log(message)
