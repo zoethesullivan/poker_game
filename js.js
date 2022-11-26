a106db407c334641dc6dd578a495325fe2617a22
Zoe <70442338+zoe-moment@users.noreply.github.com>
2022-11-26 11:34:56 -0800

    add sumEl to HTML and JS

--- js.js
+++ js.js
@@ -1,14 +1,16 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 1
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
 let messageEl = document.getElementById('message-el')
-
+let sumEl = document.getElementById('sum-el')
+console.log(sumEl)
 
 function startGame() {
+
   if (sum <= 20) {
     message = "Do you want to draw a new card?"
   } else if (sum === 21) {
@@ -18,5 +20,6 @@ function startGame() {
     message = "Busted!"
     isAlive = false
   }
+  sumEl.textContent += " " + sum
   messageEl.textContent = message
 }
