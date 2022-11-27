ccf37f5f227f544f3903569986af542226b47cf6
Zoe <70442338+zoe-moment@users.noreply.github.com>
2022-11-26 16:35:12 -0800

    add array cards to cardsEl

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 10
+let cards = [firstCard, secondCard] //array of cards delt
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -13,7 +14,7 @@ function startGame() {
   renderGame()
 }
 function renderGame() {
-  cardsEl.textContent = "Cards: " + firstCard + " & " + secondCard
+  cardsEl.textContent = "Cards: " + cards[0] + " ~ " + cards[1]
   sumEl.textContent = "sum: " + sum
   if (sum <= 20) {
     message = "Do you want to draw a new card?"
@@ -28,9 +29,9 @@ function renderGame() {
 }
 
 function newCard() {
-  console.log("drawing from deck")
-  let card = 1
+  let card = 6
   sum += card
-  sumEl.textContent = "Sum: " + (sum + newCard)
+  // Push the card to the cards array
+  cards.push(card)
   renderGame()
 }
