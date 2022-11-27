b55640c71af60798ce81df36624f082faf0e2138
Zoe <70442338+zoe-moment@users.noreply.github.com>
2022-11-26 19:22:31 -0800

    add for loop for cardsEl

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
-let firstCard = 10
-let secondCard = 10
+let firstCard = 6
+let secondCard = 9
 let cards = [firstCard, secondCard] //array of cards delt
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -13,8 +13,14 @@ let cardsEl = document.getElementById('cards-el')
 function startGame() {
   renderGame()
 }
+
 function renderGame() {
-  cardsEl.textContent = "Cards: " + cards[0] + " ~ " + cards[1]
+  cardsEl.textContent = "Cards: "
+  
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " " 
+  }
+  
   sumEl.textContent = "sum: " + sum
   if (sum <= 20) {
     message = "Do you want to draw a new card?"
@@ -29,9 +35,8 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = 6
+  let card = 1
   sum += card
-  // Push the card to the cards array
   cards.push(card)
   renderGame()
 }
