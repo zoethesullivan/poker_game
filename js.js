92588350e6949a4981cd5d020e3d60ff18bab24d
Zoe <70442338+zoe-moment@users.noreply.github.com>
2022-12-01 13:27:08 -0800

    render player name and chips on screen

--- js.js
+++ js.js
@@ -13,8 +13,7 @@ let sumEl = document.getElementById('sum-el')
 let cardsEl = document.getElementById('cards-el')
 let playerEl = document.getElementById('player-el')
 
-playerEl.textContent = player.name
-playerEl.textContent += player.chips
+playerEl.textContent = player.name + ": $" + player.chips
 
 function startGame() {
   isAlive = true
