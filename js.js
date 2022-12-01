fab18bb8cce7809f23356ffdb2d8d379ac44cb34
Zoe <70442338+zoe-moment@users.noreply.github.com>
2022-12-01 14:26:26 -0800

    correct spacing

--- js.js
+++ js.js
@@ -1,22 +1,22 @@
 let player = {
   name: "Bubba",
-  chips: 10
+  chips: 200
 }
+
 let cards = [] //array of cards
 let sum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
-
 let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById('sum-el')
 let cardsEl = document.getElementById('cards-el')
 let playerEl = document.getElementById('player-el')
 
-playerEl.textContent = player.name + ": $" + player.chips;
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
-  let randomNumber = Math.floor(Math.random() * 13) + 1
+  let randomNumber = Math.floor(Math.random() * 13) + 1 ;
   if (randomNumber > 10) {
     return 10
   } else if (randomNumber === 1) {
@@ -30,18 +30,17 @@ function startGame() {
   isAlive = true
   let firstCard = getRandomCard()
   let secondCard = getRandomCard()
-  cards = [firstCard, secondCard]
+  cards = [firstCard, secondCard] // array of cards ln6
   sum = firstCard + secondCard
   renderGame()
 }
 
-
 function renderGame() {
   cardsEl.textContent = "Cards: "
-  
-  for (let i = 0; i < cards.length; i++) {
+  for (let i = 0; i < cards.length; i++) { //cards is the array
     cardsEl.textContent += cards[i] + " " 
   }
+  
   sumEl.textContent = "sum: " + sum
   if (sum <= 20) {
     message = "Do you want to draw a new card?"
@@ -55,10 +54,12 @@ function renderGame() {
   messageEl.textContent = message
 }
 
+
 function newCard() {
   if (isAlive === true && hasBlackJack === false) {
     let card = getRandomCard()
     sum += card
     cards.push(card)
+    renderGame()
   }
 }
